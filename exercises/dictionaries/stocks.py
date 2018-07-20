0dfd419248ac50660eb8ad8113b495b980ee6c11
dan beecroft <binauralbeat001@gmail.com>
2018-07-20 11:14:22 -0500

    just updating my repos

---
+++ exercises/dictionaries/stocks.py
@@ -0,0 +1,24 @@
+word=("a string")
+players={
+    "greg": 3,
+    "doug": 83,
+    "kenith": "wizard"
+}
+
+print(players)
+
+fruits={
+    "apple": 13,
+    "organce":5,
+    "banana": 8,
+    "apricot": 3,
+    "grapes": 34
+}
+
+squares = []
+for x in range(10):
+    squares.append(x**2)
+
+print(squares)
+
+
