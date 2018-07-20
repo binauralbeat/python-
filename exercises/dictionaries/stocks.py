d703589d9f97563181f7c1f5e4c2670e0eba27ac
dan beecroft <binauralbeat001@gmail.com>
2018-07-20 11:59:42 -0500

    final commit

--- exercises/dictionaries/stocks.py
+++ exercises/dictionaries/stocks.py
@@ -5,7 +5,7 @@ purchases = [ ( 'GE', 100, '10-sep-2001', 48 ),
  ( 'GE', 200, '1-jul-1998', 56 ) ]
 
 
-def report(list=purchases, stocks=stock_dict):
+def total_purchase(list=purchases, stocks=stock_dict):
 	lump_sum = []
 	for stock in list:
 		name = stocks[stock[0]]
@@ -13,7 +13,7 @@ def report(list=purchases, stocks=stock_dict):
 		lump_sum.append({name, value})
 	return lump_sum
 
-def report_by_ticker(list=purchases, stocks=stock_dict):
+def names_stocks(list=purchases, stocks=stock_dict):
 	stock_names = {}
 	for stock in list:
 		name = stocks[stock[0]]
