8e2a29020318c259f5f6de7c7f5d6a8261d314c4
dan beecroft <binauralbeat001@gmail.com>
2018-07-20 14:16:55 -0500

    added more to planet lists

--- exercises/lists/planets.py
+++ exercises/lists/planets.py
@@ -1,5 +1,8 @@
 planet_list = ["Mercury", "Mars"]
 new_planets = "Jupiter", "Saturn"
 planet_list.extend(new_planets)
+planet_list.insert(2,"Venus")
+planet_list.insert(1, "Earth")
 
+spacecraft_list = tuple("Apple", "Mercury", "Mars")
 print(planet_list)
