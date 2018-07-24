879893c5a921f7805b4a94aa19984f2773c82a64
dan beecroft <binauralbeat001@gmail.com>
2018-07-24 15:17:22 -0500

    bag o loot

---
+++ exercises/cli/loot_test.py
@@ -0,0 +1,34 @@
+import unittest
+import sys
+from lootbag import Loot_bag
+
+
+class Kids:
+    def _init_(self, name):
+        self.name = name
+
+        self.loot = set()
+class Loot:
+    def _init_(self, loot):
+        self.loot = Loot
+
+        self.kids = set()
+
+if _name_ == '_main_':
+    kite = Loot()
+    frisbee = Loot()
+    dog = Loot()
+
+    wilbur = Kids(Wilbur)
+    joyce = Kids(Joyce)
+    hampton = Kids(Hampton)
+
+    kite.kids.add(wilbur)
+
+    print(Loot)
+
+
+
+
+
+
---
+++ exercises/cli/lootbag.py
@@ -0,0 +1,15 @@
+class Kid:
+
+    def __init__(self, name):
+        self.kid = kid
+        self.loot = []    # creates a new empty list for each Kid
+
+    def add_loot(self, loot):
+        self.kid.append(loot)
+
+    d = Kid('Emily')
+    e = Kid('Ralph')
+    d.add_loot('wasps')
+    e.add_loot('broken toy')
+
+print(d)
