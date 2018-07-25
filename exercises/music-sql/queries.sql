7b551ed9a381d5792476cd62c5925a400f684da0
dan beecroft <binauralbeat001@gmail.com>
2018-07-25 10:06:55 -0500

    music-sql

---
+++ exercises/music-sql/queries.sql
@@ -0,0 +1,12 @@
+SELECT * FROM Genre
+
+SELECT * FROM Artist;
+INSERT INTO Artist (artistName, YearEstablished)
+VALUES ('Bibio', '2000')
+SELECT * FROM Artist
+
+
+INSERT INTO Album (Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId)
+VALUES ('Ambivilance Ave', '2010', '30', 'Warp', '28', '15')
+
+
